5cc30d1d218069280c82f8032d86a4211172e731
Printed <108233540+printedz@users.noreply.github.com>
2023-05-06 17:02:19 +0000

    Clojure template for future ClojureScript, replaced Yandex

---
+++ test/src/test/core.clj
@@ -0,0 +1,9 @@
+(ns test.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+  (str "It was the panda " "in the library " "with a dust buster")
+
