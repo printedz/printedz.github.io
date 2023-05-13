72cf2ec55827485e6e5aad3cd59bb2bbc34e0a8c
Printed <108233540+printedz@users.noreply.github.com>
2023-05-13 16:22:37 +0000

    x2

--- assets/telemetry.js
+++ assets/telemetry.js
@@ -8,21 +8,28 @@ function registerScript(scriptPath) {
     s.parentNode.insertBefore(scriptTag, s);
 }
 
-const telemetryOn = document.createElement("p");
-const telemetryOff = document.createElement("p");
-const telemetryDiv = document.querySelector(".telemetry");
-
-telemetryOff.textContent = "Telemetry disabled, DNT header found";
-telemetryOn.textContent = "Telemetry enabled, DNT header not found";
-
 if (navigator.doNotTrack == 1) {
-    console.log("Telemetry disabled, DNT header found")
-    console.log("https://www.eff.org/issues/do-not-track")
-    telemetryDiv.appendChild(telemetryOff);
+    console.log("Telemetry disabled, DNT header found");
+    console.log("https://www.eff.org/issues/do-not-track");
 } else {
-    registerScript("//gc.zgo.at/count.js")
-    console.log("SAY HI TO TRACKING!! ENABLE DO NOT TRACK HEADERS IN YOUR WEB BROWSER TO DISABLE IT!!")
-    console.log("https://www.eff.org/issues/do-not-track")
-} 
+    registerScript("//gc.zgo.at/count.js");
+    console.log("SAY HI TO TRACKING!! ENABLE DO NOT TRACK HEADERS IN YOUR WEB BROWSER TO DISABLE IT!!");
+    console.log("https://www.eff.org/issues/do-not-track");
+}
 
-// document.querySelector(".telemetry").
+
+// if (navigator.doNotTrack == 1) {
+//     console.log("Telemetry disabled, DNT header found");
+//     console.log("https://www.eff.org/issues/do-not-track");
+//     telemetry.append("Telemetry disabled, DNT header found");
+
+//     const message = document.createElement("p");
+//     message.textContent = "Telemetry disabled, DNT header found";
+
+//     telemetry.appendChild(message);
+// } else {
+//     registerScript("//gc.zgo.at/count.js")
+//     console.log("SAY HI TO TRACKING!! ENABLE DO NOT TRACK HEADERS IN YOUR WEB BROWSER TO DISABLE IT!!")
+//     console.log("https://www.eff.org/issues/do-not-track")
+// } 
+
---
+++ assets/warning.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const telemetry = document.getElementsByClassName("telemetry")[0];
+    if (navigator.doNotTrack == 1) {
+        const message = document.createElement("p");
+        message.textContent = "Telemetry disabled, DNT header found";
+        message.style.setProperty("font-weight", "bold");  
+        message.style.setProperty("text-align", "center");
+        telemetry.appendChild(message); } else {
+        const message = document.createElement("p");
+        message.textContent = "Telemetry enabled, DNT header not found";
+        message.style.setProperty("font-weight", "bold");
+        message.style.setProperty("text-align", "center");
+        telemetry.appendChild(message); }
+});
