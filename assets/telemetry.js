f362415e88a52343fb4a52d9a8501e0a0266d559
Printed <108233540+printedz@users.noreply.github.com>
2023-05-13 16:22:24 +0000

    minor informative changes to the index & improved the telemetry js

--- assets/telemetry.js
+++ assets/telemetry.js
@@ -8,11 +8,21 @@ function registerScript(scriptPath) {
     s.parentNode.insertBefore(scriptTag, s);
 }
 
+const telemetryOn = document.createElement("p");
+const telemetryOff = document.createElement("p");
+const telemetryDiv = document.querySelector(".telemetry");
+
+telemetryOff.textContent = "Telemetry disabled, DNT header found";
+telemetryOn.textContent = "Telemetry enabled, DNT header not found";
+
 if (navigator.doNotTrack == 1) {
     console.log("Telemetry disabled, DNT header found")
     console.log("https://www.eff.org/issues/do-not-track")
+    telemetryDiv.appendChild(telemetryOff);
 } else {
     registerScript("//gc.zgo.at/count.js")
     console.log("SAY HI TO TRACKING!! ENABLE DO NOT TRACK HEADERS IN YOUR WEB BROWSER TO DISABLE IT!!")
     console.log("https://www.eff.org/issues/do-not-track")
 } 
+
+// document.querySelector(".telemetry").
