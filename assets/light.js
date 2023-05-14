6b124413a1fe740b21283d257d0ad80cb3a20811
Printed <108233540+printedz@users.noreply.github.com>
2023-05-14 16:53:19 +0000

    dark/light toggle everywhere embed icons??? cleaned the code

---
+++ assets/light.js
@@ -0,0 +1,9 @@
+function lightMode() {
+    var header = document.getElementsByTagName('head')[0];
+    var styleSheet = document.createElement('link');
+    styleSheet.rel = 'stylesheet';
+    styleSheet.type = 'text/css';
+    styleSheet.href = '/assets/style_light.css'; // name of your css file
+    styleSheet.media = 'all';
+    header.appendChild(styleSheet);
+}
